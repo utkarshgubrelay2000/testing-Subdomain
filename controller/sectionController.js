9dfb1a4bf4d8a416185ec44157534e1aeab471e3
Utkarsh Gubrelay <utkarshgubrelay2000@gmail.com>
2022-03-09 15:27:57 +0530

    home banner

--- controller/sectionController.js
+++ controller/sectionController.js
@@ -5,18 +5,17 @@ const { getSubDomain,checkSubDomainExist } = require("../services/subdomainServi
 
 exports.getSectionById = async (req, res) => {
  
-    let {section,id} =req.params;
+    let {id} =req.params;
     let subdomain=await getSubDomain(req.get('origin'))
     // get subdomain
     id=ObjectId(id)
-    
-    // if(await checkSubDomainExist(subdomain))
-    // return res.status(404).json({error:true, message: "No subdomain found with in Database" });
     if(subdomain=='localhost')
       return  res.status(200).json({error:true, data: "Wrong Url" });
       try {
           console.log('start');
-          let sectionCollection=await baseModel.mongoConnect(subdomain,section)
+        //  let sectionCollection=await baseModel.mongoConnect(subdomain,section)
+          let sectionCollection=await baseModel.mongoConnect(subdomain,'homepage')
+
           const result =await sectionCollection.findOne({_id:id});
           console.log('stop');
 
@@ -28,7 +27,7 @@ exports.getSectionById = async (req, res) => {
 
 };
 exports.getSection = async (req, res) => {
-    const {section} =req.params;
+   
     let subdomain=await getSubDomain(req.get('origin'))
     console.log('subdomain',subdomain);
 //    if(await checkSubDomainExist(subdomain))
@@ -36,7 +35,7 @@ exports.getSection = async (req, res) => {
     if(subdomain=='localhost')
       return  res.status(200).json({error:true, data: "Wrong Url" });
       try {
-          let sectionCollection=await baseModel.mongoConnect(subdomain,section)
+          let sectionCollection=await baseModel.mongoConnect(subdomain,'homepage')
           const result =await sectionCollection.find({}).toArray()
             res.json({ error:false, data: result });
         } catch (error) {
@@ -48,8 +47,7 @@ exports.getSection = async (req, res) => {
 
 exports.addSection = async (req, res) => {
     const sectionData =req.body;
-    const {section} =req.params;
-
+   
     let subdomain=await getSubDomain(req.get('origin'))
     console.log('subdomain',await checkSubDomainExist(subdomain));
 
@@ -59,7 +57,9 @@ exports.addSection = async (req, res) => {
       return  res.status(200).json({error:true, data: "Wrong Url" });
 
       try {
-          let sectionCollection=await baseModel.mongoConnect(subdomain,section)
+        
+          let sectionCollection=await baseModel.mongoConnect(subdomain,'homepage')
+
           const result =await sectionCollection.insertOne({
               ...sectionData,
             });
@@ -72,14 +72,16 @@ exports.addSection = async (req, res) => {
 };
 exports.editSection = async (req, res) => {
     const sectionData =req.body;
-    let {section,id} =req.params;
+    let {id} =req.params;
     let subdomain=await getSubDomain(req.get('origin'))
     if(await checkSubDomainExist(subdomain))
     return res.status(404).json({error:true, message: "No subdomain found with in Database" });
     if(subdomain=='localhost')
       return  res.status(200).json({error:true, data: "Wrong Url" });
       try {
-          let sectionCollection=await baseModel.mongoConnect(subdomain,section)
+      //    let sectionCollection=await baseModel.mongoConnect(subdomain,section)
+          let sectionCollection=await baseModel.mongoConnect(subdomain,'homepage')
+
           id=ObjectId(id)
           const result =await sectionCollection.findOneAndUpdate({_id:id},{$set:sectionData});
           console.log('result',result,id);
--- routes/section.js
+++ routes/section.js
@@ -2,16 +2,16 @@ var express = require('express');
 var router = express.Router();
 var sectionController=require('../controller/sectionController')
 /* GET home page. */
-router.post('/:section',sectionController.addSection,err=>{
+router.post('/',sectionController.addSection,err=>{
   console.log('error while signup user')
 })
-router.put('/:section/:id',sectionController.editSection,err=>{
+router.put('/:id',sectionController.editSection,err=>{
   console.log('error while signup user')
 })
-router.get('/:section',sectionController.getSection,err=>{
+router.get('/',sectionController.getSection,err=>{
     console.log('error while signup user')
   })
-  router.get('/:section/:id',sectionController.getSectionById,err=>{
+  router.get('/:id',sectionController.getSectionById,err=>{
     console.log('error while signup user')
   })
 
