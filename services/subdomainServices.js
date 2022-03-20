4b31287ca52ef89b59d83c16e547600460aa5288
Utkarsh Gubrelay <utkarsh@thebrandwick.com>
2022-03-20 08:36:23 +0530

    subdomain

--- services/subdomainServices.js
+++ services/subdomainServices.js
@@ -2,10 +2,16 @@ const baseModel = require("../model/baseModel");
 
 exports.getSubDomain=async(reqHost)=>{
    // console.log('reqHost',reqHost);
+   try {
+     
+  
     let subdomain=reqHost?.split('.')[0];
     subdomain=subdomain?.replace('https://','');
     subdomain=subdomain?.replace('http://','');
     return subdomain
+  } catch (error) {
+     console.log('error',error);
+  }
 }
 exports.checkSubDomainExist=async(subdomain)=>{
     let edtechAdminDb=await baseModel.mongoConnect('edtechAdmin','users')
