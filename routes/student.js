4b5140d22d4d398e996910bcf521a445ba39f1dd
Utkarsh Gubrelay <utkarsh@thebrandwick.com>
2022-03-22 12:00:50 +0530

    verify admin

--- routes/student.js
+++ routes/student.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var sectionController=require('../controller/studentController');
-const  verifyAdmin = require('../middleware/auth');
+
 /* GET home page. */
 
-router.get('/',verifyAdmin.auth,sectionController.getSection,err=>{
+router.get('/',sectionController.getSection,err=>{
     console.log('error while signup user')
   })
-  router.get('/:id',verifyAdmin.auth,sectionController.getSectionById,err=>{
+  router.get('/:id',sectionController.getSectionById,err=>{
     console.log('error while signup user')
   })
 
