f6ca23af831909b51ceba54b8c7577a54c1ee577
utkarshgubrelay2000 <utkarsh@thebrandwick.com>
2022-04-09 00:50:21 +0530

    index file created

---
+++ index.js
@@ -0,0 +1,18 @@
+exports.authAdmin= require('./routes/admin/Auth');
+exports.sectionRouter = require('./routes/admin/section');
+exports.contactRouter = require('./routes/admin/contact');
+exports.groupRouter = require('./routes/admin/group');
+exports.courseRouter = require('./routes/admin/course');
+exports.couponRouter = require('./routes/admin/coupon');
+exports.studentRouter = require('./routes/admin/student');
+exports.studentRouter = require('./routes/admin/');
+exports.categoryRouter = require('./routes/admin/category');
+
+/// Student Routing
+exports.studentCourseRouter = require('./routes/student/course');
+exports.userAuthRouter = require('./routes/student/userAuth');
+exports.studentCategoryRouter = require('./routes/student/category');
+exports.brandRouter = require('./routes/admin/brand');
+exports.userRouter = require('./routes/admin/user');
+exports.orderRouter = require('./routes/student/user');
+exports.sectionRouter = require('./routes/student/section');
