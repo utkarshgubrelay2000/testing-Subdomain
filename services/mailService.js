5f652686ec3dbb3fa9bd581759d852eaed44e4c6
utkarshgubrelay2000 <utkarsh@thebrandwick.com>
2022-04-09 00:15:36 +0530

    remove console

--- services/mailService.js
+++ services/mailService.js
@@ -1,7 +1,6 @@
 const nodemailer =require("nodemailer")
 const AWS =require("aws-sdk")
 
-console.log(process.env.AWS_ACCESS_KEY_ID)
 AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
